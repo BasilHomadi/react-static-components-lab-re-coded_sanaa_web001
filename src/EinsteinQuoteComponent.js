c57bb895e61949d687f004016c928189978b04f0
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 14:54:07 +0000

    Automatically backed up by Learn

---
+++ src/EinsteinQuoteComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class EinsteinQuoteComponent extends Component {
+	render() {
+		return (
+			<div className="bar" id="einstein">
+				<i>
+					<p>
+						&quot;Life is like riding a bicycle. To keep your balance you must
+						keep moving&quot;
+					</p>
+				</i>
+				<br />
+				&#9;-Einstein
+			</div>
+		);
+	}
+}
+
+export default EinsteinQuoteComponent
